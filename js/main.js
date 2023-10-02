6d5e5995dd784ec5a7930f1a1fe2aea9e4fe0e81
Mohit9486 <mohitagrawal94140@gmail.com>
2023-10-02 22:38:42 +0530

    updating structure

---
+++ js/main.js
@@ -0,0 +1,102 @@
+(function ($) {
+    "use strict";
+
+    const showIframeBtn = document.getElementById("showIframeBtn");
+    const overlayContainer = document.querySelector(".overlay-container");
+    const videoIframe = document.getElementById("videoIframe");
+    const closeIframeBtn = document.getElementById("closeIframeBtn");
+
+    showIframeBtn.addEventListener("click", () => {
+        // Set the video URL from your drive here
+        const videoURL = "your_video_drive_url_here";
+
+        // Set the iframe src attribute to the video URL
+        videoIframe.src = videoURL;
+
+        // Show the overlay container
+        overlayContainer.style.display = "block";
+    });
+
+    closeIframeBtn.addEventListener("click", () => {
+        // Hide the overlay container when the close button is clicked
+        overlayContainer.style.display = "none";
+    });
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
